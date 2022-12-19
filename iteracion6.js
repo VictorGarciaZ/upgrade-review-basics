25a8d6ab4cdbb062259d8b490799590e7765f4a7
victorgarciaz <victor.garcia@bootcamp-upgrade.com>
2022-12-19 10:13:27 +0100

    iteracion6.js

--- iteracion6.js
+++ iteracion6.js
@@ -6,4 +6,28 @@ Retorna el array resultante.
 
 Sugerencia de array:*/
 
-['Mesirve', 'Cristiano Romualdo', 'Fernando Muralla', 'Ronalguiño']
+const array = ['Mesirve', 'Cristiano Romualdo', 'Fernando Muralla', 'Ronalguiño']
+
+function swap (array, posicion1, posicion2){
+    const primero = array[posicion1];
+    const segundo = array[posicion2];
+
+    array.splice (posicion1, 1, segundo);
+    array.splice(posicion2, 1, primero)
+
+    return array 
+}
+
+console.log(swap(array, 0, 3));
+
+
+////
+function swap(array, posicion1, posicion2)[
+   
+    const primero = array [posicion1];
+
+    array[posicion1] = array[posicion2]
+    array[posicion2] = primero;
+    
+    return array
+]
